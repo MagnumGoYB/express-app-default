c18e3965090973e4428d161ea609d32c4ad8e575
MagnumGoYB <sosbs@outlook.com>
2018-09-29 21:02:31 +0800

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+var fs = require('fs');
+
+router.get('/', function(req, res, next) {
+  res.sendFile('index.html');
+});
+
+module.exports = router;
